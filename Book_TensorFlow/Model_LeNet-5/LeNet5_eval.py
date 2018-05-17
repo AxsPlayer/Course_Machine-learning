26e897289df876b4554501f37e3c493bb318123b
AxsPlayer <skyprince213@gmail.com>
2018-05-17 19:43:06 +0800

    Update model script

--- Book_TensorFlow/Model_LeNet-5/LeNet5_eval.py
+++ Book_TensorFlow/Model_LeNet-5/LeNet5_eval.py
@@ -42,4 +42,43 @@ def evaluate(mnist):
         validate_feed = {x: mnist.validation.images,
                          y_: mnist.validation.labels}
 
+        # Calculate inference result, as well as accuracy of model.
+        y = LeNet5_inference.inference(x, False, None)
+        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+        # Set mode of rename to load model, so as to deal with moving average variable problems.
+        variable_averages = tf.train.ExponentialMovingAverage(LeNet5_train.MOVING_AVERAGE_DECAY)
+        variable_to_restore = variable_averages.variables_to_restore()
+        saver = tf.train.Saver(variable_to_restore)
+
+        # Evaluate performance of model in fixed interval.
+        while True:
+            with tf.Session() as sess:
+                # Find latest model file and load model.
+                ckpt = tf.train.get_checkpoint_state(LeNet5_train.MODEL_SAVE_PATH)
+                if ckpt and ckpt.model_checkpoint_path:
+                    # Load model.
+                    saver.restore(sess, ckpt.model_checkpoint_path)
+                    # Fetch global steps in file name.
+                    global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+                    accuracy_score = sess.run(accuracy, feed_dict=validate_feed)
+                    # Print the accuracy result.
+                    print("After %s training step(s), validation accuracy = %g" % (global_step, accuracy_score))
+                else:
+                    print('No checkpoint file found.')
+                    return
+            # Set interval sleep time.
+            time.sleep(EVAL_INTERVAL_SECS)
+
+
+def main():
+    mnist = input_data.read_data_sets("/path/to/mnist_data", one_hot=True)
+    evaluate(mnist)
+
+
+if __name__ == '__main__':
+    tf.app.run()
+
+
 
