0ef170053557c1031e45c3a4e92ede0b8b49164c
AxsPlayer <axsplyer@126.com>
2018-11-26 09:58:47 +0800

    Update assignment 1.

--- Stanford_Course/CS231n/assignment1/cs231n/classifiers/k_nearest_neighbor.py
+++ Stanford_Course/CS231n/assignment1/cs231n/classifiers/k_nearest_neighbor.py
@@ -122,7 +122,9 @@ class KNearestNeighbor(object):
     # HINT: Try to formulate the l2 distance using matrix multiplication    #
     #       and two broadcast sums.                                         #
     #########################################################################
-    
+    dists = np.sqrt(-2*np.dot(X, self.X_train.T) + \
+                    np.sum(np.square(self.X_train), axis = 1) + \
+                    np.transpose([np.sum(np.square(X), axis = 1)]))
     #########################################################################
     #                         END OF YOUR CODE                              #
     #########################################################################
