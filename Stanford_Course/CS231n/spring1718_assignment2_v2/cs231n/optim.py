fd6dfe847768c3eaab244c9615340e5907671c02
AxsPlayer <axsplyer@126.com>
2018-12-21 10:28:52 +0800

    Update assignment 2.

--- Stanford_Course/CS231n/spring1718_assignment2_v2/cs231n/optim.py
+++ Stanford_Course/CS231n/spring1718_assignment2_v2/cs231n/optim.py
@@ -100,7 +100,8 @@ def rmsprop(w, dw, config=None):
     # in the next_w variable. Don't forget to update cache value stored in    #
     # config['cache'].                                                        #
     ###########################################################################
-    pass
+    config['cache'] = config['decay_rate'] * config['cache'] + (1 - config['decay_rate']) * (dw**2)
+    next_w = w - config['learning_rate'] * dw / (np.sqrt(config['cache']) + config['epsilon'])
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -140,7 +141,12 @@ def adam(w, dw, config=None):
     # NOTE: In order to match the reference output, please modify t _before_  #
     # using it in any calculations.                                           #
     ###########################################################################
-    pass
+    config['t'] += 1
+    config['m'] = config['beta1'] * config['m'] + (1 - config['beta1']) * dw
+    config['v'] = config['beta2'] * config['v'] + (1 - config['beta2']) * (dw**2)
+    mb = config['m'] / (1 - config['beta1']**config['t'])
+    vb = config['v'] / (1 - config['beta2']**config['t'])
+    next_w = w - config['learning_rate'] * mb / (np.sqrt(vb) + config['epsilon'])
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
